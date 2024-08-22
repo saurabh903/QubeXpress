56f42e87eedd04c43d6497496d21802aa590777a
saurabh903 <saurabhprasad903@gmail.com>
2024-08-22 15:52:57 +0530

    commit

---
+++ Courier/script.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function () {
+    ScrollReveal().reveal('.head', {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '50px',
+        opacity: 0,
+        interval: 200
+    });
+
+    ScrollReveal().reveal('.banner-content', {
+        duration: 1000,
+        origin: 'left',
+        distance: '50px',
+        opacity: 0
+    });
+
+    ScrollReveal().reveal('.order-box', {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '50px',
+        opacity: 0
+    });
+
+    ScrollReveal().reveal('.content', {
+        duration: 1000,
+        origin: 'left',
+        distance: '50px',
+        opacity: 0
+    });
+
+    ScrollReveal().reveal('.cards', {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '50px',
+        opacity: 0,
+        interval: 200
+    });
+
+    ScrollReveal().reveal('.footer', {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '50px',
+        opacity: 0
+    });
+});
